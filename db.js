f83a797f91c9ccfd2c1041dfbdef47a194367bb1
Cuvellier Nathan <cuvellier.nathan@gmail.com>
2020-02-14 10:25:49 +0100

    add helmet and dotenv

---
+++ db.js
@@ -0,0 +1,18 @@
+let mysql = require('mysql')
+
+let con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+  
+  con.connect((err) => {
+    if(err){
+      console.log(err)
+      return;
+    }
+    console.log('Connection OK')
+  });
+
+  module.exports = con
--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-let express = require('express');
+let express = require('express')
 let session = require('express-session')
+const helmet = require('helmet')
+require('dotenv').config()
+//let con = require('./db')
+
+
+let app = express();
 app.use(express.static('public'));
 
+app.use(helmet())
+
+
 app.get('/', function(req, res) {
-    res.render(__dirname + '/public/index.ejs',);
+    res.render(__dirname + '/public/index.ejs');
 });
+
+/*********************************/
+/*             LOGIN             */
+/*********************************/
+app.get('/login', (req, res) => {
+    res.render(__dirname + '/public/auth/login.ejs');
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+/*********************************/
+/*             LOGIN             */
+/*********************************/
+app.get('/logout', (req, res) => {
+    res.redirect('/')
+})
+
+/*********************************/
+/*             LOGIN             */
+/*********************************/
+app.get('/ticket/add', (req, res) => {
+    res.render(__dirname + '/public/ticket/add.ejs');
+})
+
+app.post('/ticket/add', (req, res) => {
+    
+})
+
 
 app.listen(8080);
